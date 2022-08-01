0ac12b6ff53918b2b807dc7360464ddbf7b412da
maciejo117 <maciej.kutyla9@gmail.com>
2022-08-01 20:34:32 +0200

    Added AppBar and Footer

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./components/AppBar/AppBar";
+import Footer from "./components/Footer/Footer";
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <Header></Header>
+        <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.scss';
+
+const Footer = () => {
+    return <div className='footer'>
+        <span className='footer__text'>Find the best substitute for your meal!</span>
+    </div>
+}
+
+export default Footer;
